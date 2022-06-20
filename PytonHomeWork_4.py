c2f2a951d4e75c298c3dce9bde9586a348424f96
SlavaGr <Slavag76@yandex.ru>
2022-06-20 14:36:39 +0300

    HomeWork4 2

--- PytonHomeWork_4.py
+++ PytonHomeWork_4.py
@@ -13,3 +13,9 @@ try:
 except ValueError:
     print("Введите три целых числа через пробел")
 
+# ******************************** 1 **********************************
+
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+max_pred = [my_list[k] for k in range(1, len(my_list)) if my_list[k] > my_list[k - 1]]
+print(max_pred)
+
